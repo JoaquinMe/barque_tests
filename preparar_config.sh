69dfd17d29ba092c6d2fb72de44a83007fa992f5
Joaquin Messano <joaquin.messano@gmail.com>
2025-01-23 17:26:56 -0300

    rm antes de cp

--- preparar_config.sh
+++ preparar_config.sh
@@ -1,5 +1,9 @@
 marker=$1
 
 git clone https://github.com/JoaquinMe/barque "$marker"/barque
+
+rm "$marker"/barque/14_tests/"$marker"/"$marker"_barque_config.sh
+rm "$marker"/barque/14_tests/"$marker"/"$marker"_primers.csv
+
 cp "$marker"/barque/14_tests/"$marker"/"$marker"_barque_config.sh "$marker"/barque/02_info/barque_config.sh
 cp "$marker"/barque/14_tests/"$marker"/"$marker"_primers.csv "$marker"/barque/02_info/primers.csv
