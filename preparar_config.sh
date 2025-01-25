c6c59bbd05b16dedffa021da54f6282d47417a47
Joaquin Messano <joaquin.messano@gmail.com>
2025-01-25 16:58:15 -0300

    Update preparar_config.sh

--- preparar_config.sh
+++ preparar_config.sh
@@ -2,8 +2,8 @@ marker=$1
 
 git clone https://github.com/JoaquinMe/barque "$marker"/barque
 
-rm "$marker"/02_info/barque_config.sh
-rm "$marker"/02_info/primers.csv
+rm "$marker"/barque/02_info/barque_config.sh
+rm "$marker"/barque/02_info/primers.csv
 
 cp "$marker"/barque/14_tests/"$marker"/"$marker"_barque_config.sh "$marker"/barque/02_info/barque_config.sh
 cp "$marker"/barque/14_tests/"$marker"/"$marker"_primers.csv "$marker"/barque/02_info/primers.csv
